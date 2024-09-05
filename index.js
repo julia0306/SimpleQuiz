f3309b7a94a6b24d5c6c063a1d6492b15e329dc8
julia0306 <123451628+julia0306@users.noreply.github.com>
2024-09-05 19:05:21 +0200

    quiz functional with display of correct / incorrect answers a,nd score

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 const responses =["b", "c", "b", "b", "d", "c", "b", "c", "c", "b", ]
-
 const form = document.querySelector(".form");
+const questionDivs = document.querySelectorAll('.question-div');
+const resultsDiv = document.querySelector('.results-div');
+const submitButton = document.querySelector('.submit-button');
+const results =[];
+const radioButtons = document.querySelectorAll("input[type='radio']:checked")
 
 form.addEventListener('submit', handleSubmit)
 
-function handleSubmit(e){
+function handleSubmit(e) {
     e.preventDefault();
-    const results =[];
-    const radioButtons = document.querySelectorAll("input[type='radio']:checked")
-    
+    const radioButtons = document.querySelectorAll("input[type='radio']:checked");
     radioButtons.forEach((radioButton, index) => {
-        if((radioButton.value) === responses[index]){
-            results.push(true)
-        }else{results.push(false)}
-    }); 
-    console.log(radioButtons);
-    console.log(results)
+        const questionDiv = questionDivs[index];
+        if (radioButton.value === responses[index]) {
+            results.push(true);
+            questionDiv.style.backgroundColor = "lightgreen";
+
+        } else {
+            results.push(false);
+            questionDiv.style.backgroundColor = "lightcoral";
+        }
+    });
+    const score = results.filter(result => result === true).length;
+    displayResults(score);
+}
+
+function displayResults(score){
+    resultsDiv.classList.remove('display-none')
+    submitButton.classList.remove('display-block')
+    resultsDiv.classList.add("display-block")
+    submitButton.classList.add("display-none")
+    resultsDiv.textContent = `Your score is: ${score}/${responses.length}`;
 }
